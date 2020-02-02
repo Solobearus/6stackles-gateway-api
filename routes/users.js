ff94107d4ba75ecccc9027ff4fc873ff953d5a74
solobearus <ivansolobear@gmail.com>
2020-02-02 02:26:34 +0200

    added users routes. (not implemented yet)

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
+var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/user', function (req, res, next) {
+
 });
+router.get('/users', function (req, res, next) {
 
+});
+router.get('/user/:userid', function (req, res, next) {
+
+});
+router.put('/user', function (req, res, next) {
+
+});
+router.delete('/user', function (req, res, next) {
+
+});
 module.exports = router;
