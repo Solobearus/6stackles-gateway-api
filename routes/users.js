4d6e7dc8989df79c86990f15dee1279c617076ce
solobearus <ivansolobear@gmail.com>
2020-07-20 16:43:48 +0300

    added : get user by id route

--- routes/users.js
+++ routes/users.js
@@ -11,15 +11,26 @@ router.get('/users', function (req, res, next) {
 
 });
 
-router.get('/user/:userid', function (req, res, next) {
+router.get('/:userid', function (req, res, next) {
+    const { userid } = req.params;
+
+    fetch(`${process.env.ADRESS_OF_USERS}/${userid}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    }).then(result => result.json())
+        .then(result => {
+            console.log(`getProductByAuthorId --- ${result}`);
+            res.json(result);
+        })
+        .catch(err => res.status(500).json({ err: `Error:${err}` }));
 
 });
 
-router.put('/user/:userid', function (req, res, next) {
+router.put('/:userid', function (req, res, next) {
 
 });
 
-router.delete('/user/:userid', function (req, res, next) {
+router.delete('/:userid', function (req, res, next) {
 
 });
 
